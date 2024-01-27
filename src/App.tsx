c637158117d6b6e1f41fee7d2aa65b09747ba1d2
Juan Marcus <eng.juanmarcus@gmail.com>
2024-01-27 19:57:31 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-
+import logoImg from './assets/logo.png'
 import './App.css'
+
 
 function App() {
 
   return (
       <div>
-        <h1>teste</h1>
+        <main className='container'>
+          <img 
+              src={logoImg} 
+              alt="logo da calculadora de gasolina ou alcool" 
+          />
+          <h1 className='title'>Qual melhor opção?</h1>
+
+          <form className='form'>
+            <label>
+              Álcool (preço por litro):
+            </label>
+            <input 
+              className='input'
+              type='number' 
+              placeholder='R$'
+              step='0.01'
+              required
+            />
+          </form>
+
+          <form className='form'>
+            <label>
+              Gasolina(preço por litro):
+            </label>
+            <input 
+              className='input'
+              type='number' 
+              placeholder='R$'
+              step='0.01'
+              required
+            />
+
+            <input className="btn" type="submit" value="calcular" />
+          </form>
+
+
+        </main>
       </div>
   )
 }
